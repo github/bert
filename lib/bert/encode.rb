af79e92c91b6335a4b4620c2030ea6c0f1212e16
Charlie Somerville <charlie@charlie.bz>
2017-01-11 15:00:27 +1100

    replace Fixnum and Bignum with Integer

--- lib/bert/encode.rb
+++ lib/bert/encode.rb
@@ -70,7 +70,7 @@ module BERT
     def write_any_raw obj
       case obj
         when Symbol then write_symbol(obj)
-        when Fixnum, Bignum then write_fixnum(obj)
+        when Integer then write_integer(obj)
         when Float then write_float(obj)
         when Tuple then write_tuple(obj)
         when Array then write_list(obj)
@@ -108,7 +108,7 @@ module BERT
       write_string data
     end
 
-    def write_fixnum(num)
+    def write_integer(num)
       if num >= 0 && num < 256
         write_1 SMALL_INT
         write_1 num
