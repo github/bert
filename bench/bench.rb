b5e83ef0d846c1c40e6527712f58da63b7039d66
Matt Burke <spraints@gmail.com>
2017-05-26 14:27:43 -0400

    Serialize symbol, time, regexp in the benchmark, too
                                         user     system      total        real
    BERT v1 tiny                     0.010000   0.000000   0.010000 (  0.013280)
    BERT v1 small                    0.110000   0.000000   0.110000 (  0.116110)
    BERT v1 large                    0.630000   0.010000   0.640000 (  0.646742)
    BERT v1 complex                  2.710000   0.000000   2.710000 (  2.738346)
    BERT v1 long array               5.360000   0.000000   5.360000 (  5.391206)
    BERT v2 tiny                     0.020000   0.000000   0.020000 (  0.018202)
    BERT v2 small                    0.140000   0.000000   0.140000 (  0.133662)
    BERT v2 large                    0.640000   0.010000   0.650000 (  0.660656)
    BERT v2 complex                  2.520000   0.010000   2.530000 (  2.534638)
    BERT v2 long array               5.460000   0.010000   5.470000 (  5.520119)
    BERT v3 tiny                     0.010000   0.000000   0.010000 (  0.010693)
    BERT v3 small                    0.010000   0.000000   0.010000 (  0.011390)
    BERT v3 large                    0.220000   0.030000   0.250000 (  0.246068)
    BERT v3 complex                  0.060000   0.000000   0.060000 (  0.055431)
    BERT v3 long array               0.520000   0.000000   0.520000 (  0.522735)
    JSON tiny                        0.000000   0.000000   0.000000 (  0.005991)
    JSON small                       0.020000   0.000000   0.020000 (  0.010092)
    JSON large                       2.200000   0.020000   2.220000 (  2.252311)
    JSON complex                     0.160000   0.000000   0.160000 (  0.159742)
    JSON long array                  0.450000   0.010000   0.460000 (  0.454021)
    YAJL tiny                        0.010000   0.000000   0.010000 (  0.006357)
    YAJL small                       0.020000   0.000000   0.020000 (  0.023327)
    YAJL large                       0.960000   0.020000   0.980000 (  1.002778)
    YAJL complex                     0.280000   0.030000   0.310000 (  0.317845)
    YAJL long array                  0.750000   0.000000   0.750000 (  0.747592)
    Ruby tiny                        0.000000   0.000000   0.000000 (  0.003547)
    Ruby small                       0.010000   0.000000   0.010000 (  0.007403)
    Ruby large                       0.010000   0.000000   0.010000 (  0.012739)
    Ruby complex                     0.020000   0.000000   0.020000 (  0.015415)
    Ruby long array                  0.030000   0.000000   0.030000 (  0.036519)

--- bench/bench.rb
+++ bench/bench.rb
@@ -7,12 +7,13 @@ require 'yajl'
 require 'benchmark'
 
 ITER = 1_000
+LONG_ITER = 100
 
 tiny = t[:ok, :awesome]
 small = t[:ok, :answers, [42] * 42]
 large = ["abc" * 1000] * 100
 complex = [42, {:foo => 'bac' * 100}, t[(1..100).to_a]] * 10
-long_array = {:a => ["a"]*1000}
+long_array = {:a => ["a", :a, Time.now, /a/]*1000}
 
 Benchmark.bm(30) do |bench|
   [:v1, :v2, :v3].each do |v|
@@ -21,24 +22,24 @@ Benchmark.bm(30) do |bench|
     bench.report("BERT #{v} small") {ITER.times {BERT.decode(BERT.encode(small))}}
     bench.report("BERT #{v} large") {ITER.times {BERT.decode(BERT.encode(large))}}
     bench.report("BERT #{v} complex") {ITER.times {BERT.decode(BERT.encode(complex))}}
-    bench.report("BERT #{v} long array") {ITER.times {BERT.decode(BERT.encode(long_array))}}
+    bench.report("BERT #{v} long array") {LONG_ITER.times {BERT.decode(BERT.encode(long_array))}}
   end
 
   bench.report("JSON tiny") {ITER.times {JSON.load(JSON.dump(tiny))}}
   bench.report("JSON small") {ITER.times {JSON.load(JSON.dump(small))}}
   bench.report("JSON large") {ITER.times {JSON.load(JSON.dump(large))}}
   bench.report("JSON complex") {ITER.times {JSON.load(JSON.dump(complex))}}
-  bench.report("JSON long array") {ITER.times {JSON.load(JSON.dump(long_array))}}
+  bench.report("JSON long array") {LONG_ITER.times {JSON.load(JSON.dump(long_array))}}
 
   bench.report("YAJL tiny") {ITER.times {Yajl::Parser.parse(Yajl::Encoder.encode(tiny))}}
   bench.report("YAJL small") {ITER.times {Yajl::Parser.parse(Yajl::Encoder.encode(small))}}
   bench.report("YAJL large") {ITER.times {Yajl::Parser.parse(Yajl::Encoder.encode(large))}}
   bench.report("YAJL complex") {ITER.times {Yajl::Parser.parse(Yajl::Encoder.encode(complex))}}
-  bench.report("YAJL long array") {ITER.times {Yajl::Parser.parse(Yajl::Encoder.encode(long_array))}}
+  bench.report("YAJL long array") {LONG_ITER.times {Yajl::Parser.parse(Yajl::Encoder.encode(long_array))}}
 
   bench.report("Ruby tiny") {ITER.times {Marshal.load(Marshal.dump(tiny))}}
   bench.report("Ruby small") {ITER.times {Marshal.load(Marshal.dump(small))}}
   bench.report("Ruby large") {ITER.times {Marshal.load(Marshal.dump(large))}}
   bench.report("Ruby complex") {ITER.times {Marshal.load(Marshal.dump(complex))}}
-  bench.report("Ruby long array") {ITER.times {Marshal.load(Marshal.dump(long_array))}}
+  bench.report("Ruby long array") {LONG_ITER.times {Marshal.load(Marshal.dump(long_array))}}
 end
